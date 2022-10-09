1ee27486d3ed19a7f8ebc7df035c574861e620ba
Fredrik Salomonsson <plattfot@posteo.net>
2022-10-09 16:20:00 -0700

    Remove pinentry-rofi package definition in guix.scm
    It is in the main channel of guix, so no need to define it here.

--- guix.scm
+++ guix.scm
@@ -9,6 +9,7 @@
   (gnu packages pkg-config)
   (gnu packages texinfo)
   (gnu packages xdisorg)
+  (gnu packages gnupg)
   (ice-9 popen)
   (ice-9 rdelim)
   )
@@ -25,58 +26,6 @@
   "Skip the `.git` and `build` directory when collecting the sources."
   (let ((name (basename file)))
     (not (or (string=? name ".git") (string=? name "build")))))
-
-(define-public pinentry-rofi
-  (package
-    (name "pinentry-rofi")
-    (version "2.0.3")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/plattfot/pinentry-rofi/")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32 "044bnldz7k74s873jwsjgff176l1jsvpbaka7d1wcj8b5pwqv2av"))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:modules
-       ((ice-9 match)
-        (ice-9 ftw)
-        ,@%gnu-build-system-modules)
-       #:phases
-       (modify-phases
-           %standard-phases
-         (add-after 'install 'hall-wrap-binaries
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (bin (string-append out "/bin/"))
-                    (site (string-append out "/share/guile/site"))
-                    (rofi-bin (string-append (assoc-ref inputs "rofi") "/bin")))
-               (match (scandir site)
-                 (("." ".." version)
-                  (wrap-program
-                      (string-append bin "pinentry-rofi")
-                    (list "PATH" ":" 'prefix `(,rofi-bin)))
-                  #t)))))
-         (add-after 'compress-documentation 'installcheck
-           (lambda* rest
-             (invoke "make" "installcheck"))))))
-    (native-inputs
-     `(("autoconf" ,autoconf)
-       ("autoconf-archive" ,autoconf-archive)
-       ("automake" ,automake)
-       ("pkg-config" ,pkg-config)
-       ("texinfo" ,texinfo)))
-    (inputs `(("guile" ,guile-3.0)
-              ("rofi" ,rofi)))
-    (synopsis "Rofi GUI for GnuPG's passphrase input")
-    (description "Pinentry-rofi is a simple graphical user interface for
-passphrase or PIN when required by @code{gpg} or other software.  It is using
-the Rofi application launcher as the user interface.  Which makes it combined
-with @code{rofi-pass} a good front end for @code{password-store}.")
-    (home-page "https://github.com/plattfot/pinentry-rofi/")
-    (license license:gpl3+)))
 
 (package
   (inherit pinentry-rofi)
